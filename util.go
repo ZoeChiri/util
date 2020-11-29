866aabb2da941425e3fafd83325efe407876be15
Zoe Chirico <56097500+ZoeChiri@users.noreply.github.com>
2020-11-29 18:16:31 -0500

    Add files via upload

--- util.go
+++ util.go
@@ -27,3 +27,36 @@ func (ut *UT)DoSomething(s string){
 	ut.Mymap[s] = "Boseman"
 
 }
+
+type Node struct {
+	prev *Node
+	next *Node
+	key  interface{}
+}
+
+type List struct {
+	head *Node
+	tail *Node
+}
+func (L *List) Insert(key interface{}){
+	list := &Node{
+		next: L.head,
+		key: key,
+	}
+	if L.head != nil {
+		L.head.prev = list
+	}
+	L.head = list
+
+	l := L.head
+	for l.next != nil {
+		l = l.next
+	}
+	L.tail = l
+}
+func main(){
+	link := List{}
+	link.Insert("Nuts")
+	link.Insert("Vegan Ice Cream")
+	link.Insert("Spinach")
+}
